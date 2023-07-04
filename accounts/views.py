197fdd93877e19e1770e3a89ae87ecc1ee0b77c7
junhyukM <minjh1031@gmail.com>
2023-07-04 10:00:39 +0900

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 # Create your views here.
 
 
 def signup(request):
     if request.method == 'POST':
-        pass
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
     else:
         form = CustomUserCreationForm()
 
@@ -13,3 +16,4 @@ def signup(request):
         'form': form,
     }
     return render(request, 'accounts/form.html', context)
+
