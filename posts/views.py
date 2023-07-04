cbc5a66e0d8dfb1e559c9c62d02085191fcc84c2
junhyukM <minjh1031@gmail.com>
2023-07-04 13:26:55 +0900

    comment

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from .models import Post
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
+    comment_form = CommentForm()
 
     context = {
         'posts':posts,
+        'comment_form': comment_form,
     }
 
     return render(request, 'posts/index.html', context)
@@ -32,3 +34,15 @@ def delete(request, id):
     post = Post.objects.get(id=id)
     post.delete()
     return redirect('posts:index')
+
+
+def comments_create(request, id):
+    post = Post.objects.get(id=id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = post
+            comment.save()
+            return redirect('posts:index')
