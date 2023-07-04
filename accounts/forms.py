55ac77ee3f99994c34f119f367196f0504ac5eb5
junhyukM <minjh1031@gmail.com>
2023-07-04 09:48:20 +0900

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,5 @@ from .models import User
 from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta():
+    class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = 
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import CustomUserCreationForm
 # Create your views here.
 
 
@@ -7,4 +7,9 @@ def signup(request):
     if request.method == 'POST':
         pass
     else:
-        pass
+        form = CustomUserCreationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/form.html', context)
