f1336025cbee1fbff83d437c0623d94b351911b6
junhyukM <minjh1031@gmail.com>
2023-07-10 09:15:41 +0900

    update

--- insta/settings.py
+++ insta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!z89n)zjvwjdoij6djirx(in3p^oepbpqe^+g%^&uyfaf5xg9+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
